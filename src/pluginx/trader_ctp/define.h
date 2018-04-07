be37aa0e68033610d6b7c5c0e984825c6b92cf0e
xurendong <xrdlyj@163.com>
2018-04-07 14:09:57 +0800

    init plugin trader_ctp

---
+++ src/pluginx/trader_ctp/define.h
@@ -0,0 +1,38 @@
+/*
+* Copyright (c) 2018-2018 the TradeX authors
+* All rights reserved.
+*
+* The project sponsor and lead author is Xu Rendong.
+* E-mail: xrd@ustc.edu, QQ: 277195007, WeChat: ustc_xrd
+* You can get more information at https://xurendong.github.io
+* For names of other contributors see the contributors file.
+*
+* Commercial use of this code in source and binary forms is
+* governed by a LGPL v3 license. You may get a copy from the
+* root directory. Or else you should get a specific written
+* permission from the project author.
+*
+* Individual and educational use of this code in source and
+* binary forms is governed by a 3-clause BSD license. You may
+* get a copy from the root directory. Certainly welcome you
+* to contribute code of all sorts.
+*
+* Be sure to retain the above copyright notice and conditions.
+*/
+
+#ifndef TRADER_CTP_DEFINE_H
+#define TRADER_CTP_DEFINE_H
+
+#include "../../global/define.h"
+
+// 插件定义
+#define PLUGIN_NAME_TRADER_CTP "TraderCTP" // 插件名称
+#define PLUGIN_NAME_CN_TRADER_CTP "上 期 期 货 交 易 插 件" // 插件中文名称
+#define PLUGIN_VERSION_TRADER_CTP "V0.1.0-Beta Build 20180407" // 插件版本号
+
+// 插件自定义消息
+
+// 插件自定义参数
+#define CFG_TRADER_CTP_WORK_THREAD_NUM 1 // 输入输出服务工作线程数
+
+#endif // TRADER_CTP_DEFINE_H
