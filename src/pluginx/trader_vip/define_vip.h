e4afe7074f591011b5c0df57f55bd47c027d4234
xurendong <xrdlyj@163.com>
2018-06-28 12:12:29 +0800

    add define_vip.h and trader_vip.h

---
+++ src/pluginx/trader_vip/define_vip.h
@@ -0,0 +1,38 @@
+/*
+* Copyright (c) 2018-2018 the TradeX authors
+* All rights reserved.
+*
+* The project sponsor and lead author is Xu Rendong.
+* E-mail: xrd@ustc.edu, QQ: 277195007, WeChat: ustc_xrd
+* You can get more information at https://xurendong.github.io
+* For names of other contributors see the contributors file.
+*
+* Commercial use of this code in source and binary forms is
+* governed by a LGPL v3 license. You may get a copy from the
+* root directory. Or else you should get a specific written
+* permission from the project author.
+*
+* Individual and educational use of this code in source and
+* binary forms is governed by a 3-clause BSD license. You may
+* get a copy from the root directory. Certainly welcome you
+* to contribute code of all sorts.
+*
+* Be sure to retain the above copyright notice and conditions.
+*/
+
+#ifndef TRADER_VIP_DEFINE_H
+#define TRADER_VIP_DEFINE_H
+
+#include "../../global/define.h"
+
+// 插件定义
+#define PLUGIN_NAME "trader_vip" // 插件名称
+#define PLUGIN_NAME_CN "顶 点 快 速 股 票 交 易 插 件" // 插件中文名称
+#define PLUGIN_VERSION "V0.1.0-Beta Build 20180628" // 插件版本号
+
+// 插件自定义消息
+
+// 插件自定义参数
+#define CFG_WORK_THREAD_NUM 1 // 输入输出服务工作线程数
+
+#endif // TRADER_VIP_DEFINE_H
